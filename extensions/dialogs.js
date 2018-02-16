8e236865dffc99c839be257951db9a149743360a
Camila Lara Palomé <32206610+CamiLara@users.noreply.github.com>
2018-02-16 19:53:23 -0300

    correccion

--- extensions/dialogs.js
+++ extensions/dialogs.js
@@ -3,14 +3,10 @@ var dialogs = require('../dialogs/serviceDialogs')
 var Dialogs = {
     getDialogs: function (builder) {
         var RSHTramo = new dialogs.RSHTramo(builder);
-
-        return [
-            { dialogId: RSHTramo.dialogId, dialog: RSHTramo.dialog }, //ObtenerTramoRsh
-        ]
-
         var RCivilInfoGeneral = new dialogs.RCivilInfoGeneral(builder);
 
         return [
+            { dialogId: RSHTramo.dialogId, dialog: RSHTramo.dialog }, //ObtenerTramoRsh
             { dialogId: RCivilInfoGeneral.dialogId, dialog: RCivilInfoGeneral.dialog }, //RegistroCivilInfoGeneral
         ]
     }
